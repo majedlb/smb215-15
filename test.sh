eefac75b5d44a06bbbfb6ec4b73e3f27cc96f6a4
Pascal E Fares <pfares@cofares.net>
2015-03-06 20:36:03 +0200

    curl testing

---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -k -X PUT -H "Content-Type: text/plain"  -H "Accept: text/plain" -d "toto" http://localhost:8080/premierRest/wr/hello1
---
+++ testget.sh
@@ -0,0 +1,1 @@
+curl -k -X GET -H "Content-Type: text/plain"  -H "Accept: text/plain" -d "toto" http://localhost:8080/premierRest/wr/hello1
