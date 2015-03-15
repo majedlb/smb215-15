07e8f2b3c33f9fcec61cf5ecb2ddde8087cef58a
Pascal E Fares <pfares@cofares.net>
2015-03-15 20:59:31 +0200

    test de l'appel restfull
    Avec Liste

---
+++ restCall/src/net/cofares/objects/TestA.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.cofares.objects;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author pascalfares
+ */
+@XmlRootElement (name = "TestA")
+public class TestA implements Serializable{
+    private int idA;
+    private String valeur;
+
+    public TestA() {
+        
+    }
+    public TestA(int id, String v){
+        idA=id;
+        valeur = new String(v);
+    }
+    
+    public TestA(TestA ta){
+        idA=ta.getIdA();
+        valeur = new String(ta.getValeur());
+    }
+    /**
+     * @return the idA
+     */
+    
+    public int getIdA() {
+        return idA;
+    }
+
+    /**
+     * @param idA the idA to set
+     */
+    public void setIdA(int idA) {
+        this.idA = idA;
+    }
+
+    /**
+     * @return the valeur
+     */
+  
+    public String getValeur() {
+        return valeur;
+    }
+
+    /**
+     * @param valeur the valeur to set
+     */
+    public void setValeur(String valeur) {
+        this.valeur = valeur;
+    }
+    
+    public String toString() {
+        return "TestA: id ="+idA+" Val="+valeur+"\n";
+    }
+}
---
+++ restCall/src/restcall/RestCall.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restcall;
+
+import net.cofares.call.NewJerseyClient;
+import net.cofares.objects.TestA;
+import net.cofares.objects.TestC;
+
+/**
+ *
+ * @author pascalfares
+ */
+public class RestCall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJerseyClient restCall = new NewJerseyClient();
+        
+        //Les ping
+        System.out.println(restCall.pingGet());
+        TestA ta = restCall.pingAGet(TestA.class);
+        System.out.println(ta);
+        TestC tc = restCall.pingAGet(TestC.class);
+        System.out.println(tc);
+        
+        //Initialisation de test
+        TestC[] ltc = restCall.initGet(TestC[].class);
+        for (TestC c : ltc){
+            System.out.println(c);
+        }
+    }
+    
+}
