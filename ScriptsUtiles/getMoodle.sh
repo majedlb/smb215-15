cec36d7617f29096bd5bac07bfabb00abe71f3a5
Pascal Fares sur cnamliban <pfares@cofares.net>
2015-04-25 12:00:19 +0300

    Recuperer moodle

---
+++ ScriptsUtiles/getMoodle.sh
@@ -0,0 +1,5 @@
+git clone git://git.moodle.org/moodle.git
+cd moodle
+git branch -a                           
+git branch --track MOODLE_28_STABLE origin/MOODLE_28_STABLE
+git checkout MOODLE_28_STABLE         
