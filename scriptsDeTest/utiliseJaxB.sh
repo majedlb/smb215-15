4718e5220f1cdedf9015ee913ddbd4fa3116dd36
Pascal E Fares <pfares@cofares.net>
2015-05-05 12:35:01 +0300

    Deplacment des scritps

--- scriptsDeTest/utiliseJaxB.sh
+++ scriptsDeTest/utiliseJaxB.sh
@@ -1,6 +1,1 @@
-# $2 : json ou xml
-# $1 : Les données à envoyé (dans un fichier)
-
-service=http://localhost:8080/jb/wr/test
-
-curl -X PUT -H "Content-Type: application/json"  -H "Accept: application/json" -d @$1 $service
+curl -X PUT -H "Content-Type: application/json"  -H "Accept: application/json" -d @$1 http://localhost:8080/jb/wr/test
--- utiliseJaxB.sh
+++
@@ -1,1 +0,0 @@
-curl -X PUT -H "Content-Type: application/json"  -H "Accept: application/json" -d @$1 http://localhost:8080/jb/wr/test
